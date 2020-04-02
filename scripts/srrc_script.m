b7857040c3f722b9b0185eef2f4bf9bbeb2cecc6
Neel Drain <Darthneel@gmail.com>
2020-04-02 16:10:44 -0400

    srrc script with unity passband gain

--- scripts/srrc_script.m
+++ scripts/srrc_script.m
@@ -6,8 +6,10 @@ sampsPerSym = 6;    % Upsampling factor - aslso helps to smoothen out the graph
 
 % Parameters
 DataL = 8;             % Data length in symbols
-R = 1000;               % Data rate
+R = 500;               % Data rate
 Fs = R * sampsPerSym;   % Sampling frequency
+Fc = 9000;               %Carrier frequency, 9kHz
+n = 0:(DataL*Fs/R-1);
 
 %Example Bitstream mapped with BPSK
 %Note: must be a column vector e.g. (1,elements)
@@ -33,14 +35,32 @@ rctFilt3 = comm.RaisedCosineTransmitFilter(...
   'FilterSpanInSymbols',    Nsym, ...
   'OutputSamplesPerSymbol', sampsPerSym);
 
+%set unity passband gain and verify it's 1
+b = coeffs(rctFilt3);
+rctFilt3.Gain = 1/sum(b.Numerator);
+bNorm = coeffs(rctFilt3);
+sum(bNorm.Numerator)
+
 % Upsample and filter.
 yc = rctFilt3([x; zeros(Nsym/2,1)]);
 % Correct for propagation delay by removing filter transients
 yc = yc(fltDelay*Fs+1:end);
+%message filter * carrier wave 
+message = yc.*cos(2*pi*Fc/Fs*n);
+
+figure(1)
 % Plot data.
 stem(tx, x, 'kx'); hold on;
 % Plot filtered data.
 plot(to, yc, 'm-'); hold off;
 % Set axes and labels.
-axis([-1 DataL+1 -1.7 1.7]);  xlabel('Time (ms)'); ylabel('Amplitude');
-legend('Mapped-Bitstream', 'Sqrt. Raised Cosine', 'Location', 'southeast')
+axis([-1 DataL*2 -1.7 1.7]);  xlabel('Time (ms)'); ylabel('Amplitude');
+legend('Mapped-Bitstream', 'Sqrt. Raised Cosine', 'Location', 'southeast');
+
+
+%figure(2)
+%modulate on cosine carrier of 8kHz
+%plot(message, 'm-'); hold on;
+%axis([-1 DataL*8 -1.7 1.7]); xlabel('Time (ms)'); ylabel('Amplitude');
+%legend('Modulated SRRC Data','Location','southeast');
+
