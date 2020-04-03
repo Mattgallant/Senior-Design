0b9ed913fdee6b2fd3530543d50947297f7ae5b9
Matthew Gallant <31527991+Mattgallant@users.noreply.github.com>
2020-04-03 17:53:45 -0500

    Updated noisychan, quantalph with a few more comments

--- noisychan.m
+++ noisychan.m
@@ -8,9 +8,11 @@ p=1/15; s =1.0;                         % power of noise and signal --> SNR = s/
 x=pam(m, 4 , s ) ;                      % 4?PAM input with power 1 . . . SEE PAM FUNCTION
 L=sqrt ( 1 / 5 ) ;                      % ...with amp levels L
 n=sqrt (p)*randn(1 ,m) ;                % noise with power p
-y=x+n ;                                 % output adds noi s e to data
+y=x+n ;                                 % output adds noise to data
 qy=quantalph(y,[-3*L,-L,L,3*L]) ;       % quantize to [?3*L,?L,L,3*L]   SEE QUANTALPH FUNCTION
 err=sum(abs(sign(qy'-x )))/m;           % percent transmission errors    
 
-disp(err)
-disp(sum(x)/m)
+disp("Error rate is " + err)
+disp("SNR is " + s/p)
+%disp(err)
+%disp(sum(x)/m)
--- quantalph.m
+++ quantalph.m
@@ -7,10 +7,15 @@
 %                  sorted in ascending order
 % output y - quantized vector
 function y=quantalph(x,alphabet)
-alphabet=alphabet(:);
-x=x(:);
-alpha=alphabet(:,ones(size(x)))';
-dist=(x(:,ones(size(alphabet)))-alpha).^2;
-[v,i]=min(dist,[],2);
-y=alphabet(i);
+    alphabet=alphabet(:);
+    %disp(alphabet);
+    x=x(:);
+    %disp(ones(size(x)))
+    alpha=alphabet(:,ones(size(x)))';
+    %disp(alpha)
+    dist=(x(:,ones(size(alphabet)))-alpha).^2; %Calculating the distance of all neighbors
+    %disp(dist);
+    [v,i]=min(dist,[],2);   %Finding the min distance, stored in the index (i)
+    %disp([v, i, dist])
+    y=alphabet(i);          %Return the alphabet vector of quantized symbols
 
