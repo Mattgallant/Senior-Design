78571727da8dbd2ccf4a735872ff4c063397364f
Matthew Gallant <31527991+Mattgallant@users.noreply.github.com>
2020-04-02 20:38:22 -0500

    Starting on Channel Encoding experiment. Got VERY lost.

---
+++ ChannelEncodingExperiment/Convolutional_BPSK.m
@@ -0,0 +1,22 @@
+% Matt's part of the channel encoding risk reduction experiment
+% Source: https://www.mathworks.com/help/lte/ref/lteconvolutionaldecode.html
+
+%Generate random bit stream
+txBits = randi([0 1],100,1);
+disp(txBits)
+%Encode data
+codedData = lteConvolutionalEncode(txBits);
+
+%Modulate the codeded bits then add noise
+txSym = lteSymbolModulate(codedData,'BPSK');
+%noise = .5*complex(randn(size(txSym)),randn(size(txSym)));
+%y = x + sqrt(var)*randn(1,length(x));
+rxSym = txSym + noise;
+
+%xylimits = [-2.5 2.5];
+%cdScope = comm.ConstellationDiagram('ReferenceConstellation',txSym,'XLimits',xylimits ,'YLimits',xylimits);
+%cdScope(rxSym)
+
+softBits = lteSymbolDemodulate(rxSym,'BPSK','Soft');
+out = lteConvolutionalDecode(softBits);
+disp(sum(out ~= int8(txBits)))
