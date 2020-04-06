d35c602304422b859ee64501ea4b647d943315a1
Neel Drain <Darthneel@gmail.com>
2020-04-06 13:34:39 -0400

    new version of LDPC 16QAM script

--- ChannelEncodingExperiment/ldpc_qam16_script.m
+++ ChannelEncodingExperiment/ldpc_qam16_script.m
@@ -1,62 +1,86 @@
-%Implementation of LDPC code using 16-QAM - Adapted from MATLAB Example:
-%LDPC - QPSK code
-%Modulation order (16-QAM)
+clear; close all
+%Sets random number generator to default settings
+rng default
+%Sets the QAM level (modulation order) to 16
 M = 16;
-%Bits per symbol
+%Sets the bits per symbol
 k = log2(M);
-%Number of bits 
-n = 32400;
-%Number of samples per symbol
-L = 1;
-%SNR valuees to test
-snr = [0.25,0.5,0.75,1.0,1.25];
-%Error rate variables
-error_rate = zeros(1,length(snr));
-unc_error_rate = zeros(1,length(snr));
-%Generate random data (binary)
-rng default;
-message = randi([0 1], n, 1);
+%Sets Eb/No SNR (db) range
+SNR_db_norm = (4:10)';
 %Init. LDPC encoder and decoder
 ldpcEncoder = comm.LDPCEncoder;
-ldpcDecoder = comm.LDPCDecoder;
-%Encode data
-e_message = ldpcEncoder(message);
-%Convert encoded binary message into integer for qammod in gray coding
-%TODO Write 16QAM encoder from scratch
-e_message_matrix = reshape(e_message,length(e_message)/k,k);
-e_message_mod_input = bi2de(e_message_matrix);
-e_message_mod_gray = qammod(e_message_mod_input,M);
-%Convert un-encoded binary message into integer for qammod in gray coding
-unc_message_matrix = reshape(message,length(message)/k,k);
-unc_message_mod_input = bi2de(unc_message_matrix);
-unc_message_mod_gray = qammod(unc_message_mod_input,M);
-%Loop through the different SNR values
-for ii = 1 : length(snr)
-    %Pass signal through AWGN Channel
-    %TODO Write AWGN Channel from scratch
-    r_signal_gray = awgn(e_message_mod_gray,snr(ii),'measured');
-    unc_r_singal_gray = awgn(unc_message_mod_gray,snr(ii),'measured');
-    %Demodulate recived signal
-    e_message_demod_dec = qamdemod(r_signal_gray,M);
-    unc_message_demod_dec = qamdemod(unc_r_singal_gray,M);
-    %Convert integer-valued demodulation to binary demodulation
-    e_message_demod_matrix = de2bi(e_message_demod_dec,k);
-    e_message_demod = e_message_demod_matrix(:);
-    unc_message_demod_matrix = de2bi(unc_message_demod_dec,k);
-    unc_message_demod = unc_message_demod_matrix(:);
-    %Decode demodulated signal into bits
-    message_decode = ldpcDecoder(e_message_demod);
-    %Calcualte local and total bit error 
-    num_err = biterr(message,message_decode);
-    %total_err = total_err + num_err;
-    unc_num_err = biterr(message,unc_message_demod);
-    %unc_total_err = unc_total_err + unc_num_err;
-    error_rate(ii) = num_err / n;
-    unc_error_rate(ii) = unc_num_err  / n;
+ldpcDecoder_soft = comm.LDPCDecoder('DecisionMethod','Soft decision');
+ldpcDecoder_hard = comm.LDPCDecoder('DecisionMethod','Hard decision');
+%This program is adapted from https://www.mathworks.com/help/comm/ref/qamdemod.html
+%MATLAB's example uses frames per SNR value to aggregate more data
+%Sets symbols per frame
+sym_frame = 8100;
+%Inits. BER vectors
+BER_soft = zeros(size(SNR_db_norm));
+BER_hard = zeros(size(SNR_db_norm));
+BER_unc_hard = zeros(size(SNR_db_norm));
+%Set bitrate
+rate = 1/2;
+snr = 0.25;
+%Main loop iterating through SNR_db values
+for n = 1 : length(SNR_db_norm)
+    %Convert Eb/No SNR_db_norm to SNR 
+    %snr = SNR_db_norm(n) + 10*log10(k*rate);
+    %Use simple snr increments
+    snr = snr + 0.25;
+    %Calculate noise variance for unit power
+    noiseVar = 10.^(-snr/10);
+    %Reset error counters for use in a new snr iteration
+    [numErrsSoft,numErrsHard,numErrsUncHard,num_bits] = deal(0);
+    %Loop until a bit threshold per snr value is reached
+    while num_bits < 1e5
+        % Generate binary data and convert to symbols
+        data_in = randi([0 1],sym_frame*k,1); 
+        %LDPC encode the data_in
+        data_enc = ldpcEncoder(data_in);
+        %Modulate encoded data data_enc using 16-QAM (default: gray coding)
+        data_mod = qammod(data_enc,M,'InputType','bit','UnitAveragePower',true);
+        %Modulate unencoded data data_in using 16-QAM
+        unc_data_mod = qammod(data_in,M,'InputType','bit','UnitAveragePower',true);
+        %Pass modulated data data_mod through AWGN channel
+        channel = awgn(data_mod,snr,'measured');
+        unc_channel = awgn(unc_data_mod,snr,'measured');
+        %Demodulate the channel using hard-decision
+        %Output: bit
+        r_data_hard = qamdemod(channel,M,'OutputType','bit','UnitAveragePower',true);
+        r_unc_data_hard = qamdemod(unc_channel,M,'OutputType','bit','UnitAveragePower',true);
+        %Demodulate the channel using soft-decision
+        %Output: log-likelyhood ratio (llr)
+        r_data_soft = qamdemod(channel,M,'OutputType','approxllr', ...
+            'UnitAveragePower',true,'NoiseVariance',noiseVar);
+        %LDPC Decode demodulated hard data
+        r_data_hard_decode = ldpcDecoder_hard(r_data_hard);
+        %LDPC Decode demodulated soft data
+        r_data_soft_decode = ldpcDecoder_hard(r_data_soft);
+        %Calculate bit error in the specified frame
+        num_err_hard = biterr(data_in,r_data_hard_decode);
+        num_err_soft = biterr(data_in,r_data_soft_decode);
+        num_err_unc_hard = biterr(data_in,r_unc_data_hard);
+        %Increment bit and bit error counters
+        numErrsHard = numErrsHard + num_err_hard;
+        numErrsSoft = numErrsSoft + num_err_soft;
+        numErrsUncHard = numErrsUncHard + num_err_unc_hard;
+        num_bits = num_bits + sym_frame*k; 
+    end
+    %Estimate BER for hard and soft decision making
+    BER_soft(n) = numErrsSoft/num_bits;
+    BER_hard(n) = numErrsHard/num_bits;
+    BER_unc_hard(n) = numErrsUncHard/num_bits;
 end
-%Plot BER vs. SNR
-plot(snr,unc_error_rate,snr,error_rate);
-legend('Uncoded','LDPC Coded');
-xlabel('SNR (db)');
-ylabel('BER');
+%Plot data
+semilogy(SNR_db_norm,[BER_soft BER_hard],'-*')
+hold on
+semilogy(SNR_db_norm,BER_unc_hard)
+hold on
+semilogy(SNR_db_norm,berawgn(SNR_db_norm,'qam',M))
+legend('Soft','Hard','Uncoded','Generic Uncoded','location','best')
+grid
+xlabel('Eb/No (dB)')
+ylabel('Bit Error Rate')
+
 
