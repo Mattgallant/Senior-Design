a21c1bd4882471783d3988db600ecb5bda8d8631
AustinDuong <Duong_Austin@utexas.edu>
2020-04-10 12:22:00 -0500

    Created 2 functions, one to embed PN sequence and one to detect and plot

---
+++ AGCExperiment/functions/Embed_PNSequence.m
@@ -0,0 +1,16 @@
+%Embed a PN sequence into bitstream;
+% Paramater bitstream - input bitstream
+% Parameter splitIndex - Where we want to split bitstream and embed the
+% training sequence
+function embeddedStream = Embed_PNSequence(bitstream, splitIndex)
+    % Create Pseudonumber sequence object with certain properties
+    pnSequence = comm.PNSequence('Polynomial',[3 2 0],'SamplesPerFrame',7,'InitialConditions',[0 0 1]);
+
+    % Generate the PN training sequence
+    training_sequence = pnSequence();
+    training_sequence = training_sequence';
+    
+    % Embed training sequence into bitstream
+    s1 = bitstream(1:splitIndex);
+    s2 = bitstream(splitIndex+1:end);
+    embeddedStream = horzcat(s1,training_sequence,s2);
