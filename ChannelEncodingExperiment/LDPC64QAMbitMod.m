b46c7bc4760c784b166cbc38c3284b533a499175
Phat Le <phatle10@gmail.com>
2020-04-10 08:29:38 -0500

    finished 64 QAM LDPC encoding

--- ChannelEncodingExperiment/LDPC64QAMbitMod.m
+++ ChannelEncodingExperiment/LDPC64QAMbitMod.m
@@ -6,96 +6,131 @@ clc, clear all, close all;
 
 ldpcEnc = comm.LDPCEncoder;
 ldpcDec = comm.LDPCDecoder;
+ldpcDecSoft = comm.LDPCDecoder('DecisionMethod','Soft decision');
 
-%setting modulation order and number of data points
-M = 16;
-num = 1000;
+%setting modulation order
+M = 64;
 
-%modulate with gray encoding
-%data = pammod(dataIn,M,0,'gray');
-%dataOut = pamdemod(modData,M,0,'gray');
+% snrVec = 0:2:40;
+% ber = zeros(length(snrVec),1);
+% BER = zeros(length(snrVec),1);
 
-%scatter((1:num),modData);
+%Using Eb/No (db) range instead of snr vector
+EbNo = (-2:0.25:10)';
+ber = zeros(size(EbNo));
+BER = zeros(size(EbNo));
+BERSoft = zeros(size(EbNo));
+BERUncoded = zeros(size(EbNo));
 
-snrVec = 0:2:40;
-ber = zeros(length(snrVec),1);
-BER = zeros(length(snrVec),1);
-L = sqrt(1/8);
+%normalization factor
+L = sqrt(1/42);
 
-for k = 1:length(snrVec)
-    SNR = snrVec(k);     %in db
+%number of bits
+N = 32400/2;        %something with LDPC funtion's parameter requires this amount
+
+for k = 1:length(EbNo)
+    %get snr now from EbNo
+    bps = log2(M);    %bits per symbol
+    codeRate = 1/2;
+    %SNR = EbNo(k) + 10*log10(bps);
+    SNR = EbNo(k) * codeRate * bps;
     % Generate binary data
-    N = 32400/2;        %something with LDPC funtion's parameter 
-    txbits = randi([0 1],1,2*N);    
+    
+    txbits = randi([0 1], 2*N, 1);    
     
     % Apply LDPC encoding
-    encData = ldpcEnc(logical(txbits.'));                
+    encData = step(ldpcEnc, txbits);%ldpcEnc(logical(txbits));                
    
     % Modulate
-    dataConvert = double(encData).';
-    for i = 3:3:4*N
-            transData(i/3) = bi2de([dataConvert(i-2),dataConvert(i-1), dataConvert(i)]); %should be gray encoded
-    end
-    modSig = L*(((2*transData(1:2:end)-7)) + 1i*(2*transData(2:2:end)-7)).';    %8 by 8 what sqrt to use?
+%     dataConvert = double(encData).';
+%     for i = 3:3:4*N
+%             transData(i/3) = bi2de([dataConvert(i-2),dataConvert(i-1), dataConvert(i)]); %should be gray encoded
+%     end
+%     modSig = L*(((2*transData(1:2:end)-7)) + 1i*(2*transData(2:2:end)-7)).';   
+
+    %Using qammod instead of by hand
+    %modSig = qammod(double(encData),M,'gray'); % Gray coding with phase offset of zero
+    modSig =  qammod(double(encData),M,'InputType','bit','UnitAveragePower',true);  %why can't gray code ;-;
+    modSigUncoded = qammod(double(txbits),M,'InputType','bit','UnitAveragePower',true);
 %scatterplot(modSig)
+%scatterplot(modSigUncoded)
+
     % Define energy of transmitted signal
-    Energy_modSig = mean(abs(modSig).^2);   %this isn't 1 with ^^ sqrt
+    Energy_modSig = mean(abs(modSig).^2);     
+    Energy_modSigUncoded = mean(abs(modSigUncoded).^2);
     
     % Create noise signal and add it to received signal
-    snr = 10^(SNR/10);
-    noise_var = Energy_modSig/snr;   % snr = Energy_x/noise_var
-        
+    snr = 10.^(SNR/10);
+    noise_var = 1/snr;   % snr = Energy_x/noise_var
+    noise_varUncoded = 1/snr;   % snr = Energy_x/noise_var
+     
     % Pass through AWGN channel
-    noise = sqrt(noise_var/2)*(randn(N/3*2,1) + 1i*randn(N/3*2,1)); %!!!!!!!!!!!watch for size
-    noise_var_check = mean(abs(noise).^2);
+%     noise = sqrt(noise_var/2)*(randn(N/3*2,1) + 1i*randn(N/3*2,1)); %!!!!!!!!!!!watch for size
+%     %noise = sqrt(noise_varUncoded/2)*(randn(N/3*2,1) + 1i*randn(N/3*2,1)); %what if
+%     noise_var_check = mean(abs(noise).^2);
+%     noiseUncoded = sqrt(noise_varUncoded/2)*(randn(N/3,1) + 1i*randn(N/3,1)); %!!!!!!!!!!!watch for size
+%     noise_var_checkUncoded = mean(abs(noiseUncoded).^2);
+%     
+%      rxSig = modSig + noise; 
+%      rxSigUncoded = modSigUncoded + noiseUncoded; 
 
-    rxSig = modSig + noise;   %!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        
-    SNR_check = 10*log10(Energy_modSig/noise_var_check);
-%scatterplot(rxSig)
-
-    % Bit Error Rate
+%     codeRate = 1/2;
+%     chan = comm.AWGNChannel('NoiseMethod','Signal to noise ratio (Eb/No)', ...
+%         'BitsPerSymbol',bps);
+%     EbNoCoded = SNR + 10*log10(codeRate);
+%     chan.EbNo = EbNoCoded;
+%     
+%      rxSig = chan(modSig);
+%     rxSigUncoded = chan(modSigUncoded);
+ 
+    %cave in and use the awgn function
+    rxSig = awgn(modSig,SNR,'measured');
+    rxSigUncoded = awgn(modSigUncoded,SNR,'measured');
     
+%     SNR_check = 10*log10(Energy_modSig/noise_var_check);
+%     SNR_checkUncoded = 10*log10(Energy_modSigUncoded/noise_var_checkUncoded);
+%      scatterplot(rxSig)
+%   scatterplot(rxSigUncoded)
+
     % Step 5: Convert received signal to bits
     
-    %x_quant = 1/sqrt(8)*(sign(real(rxSig)) + 1i*sign(imag(rxSig)));
-        %hold = ((sqrt(8)*(real(rxSig)))+7)/2;
-        convertReal = quantalph(real(rxSig), L*[-7, -5, -3, -1, 1, 3, 5, 7]);
-        convertImag = quantalph(imag(rxSig), L*[-7, -5, -3, -1, 1, 3, 5, 7]);
+    %qamdemod demodulate (hard decision)
+    rec_bits = qamdemod(rxSig,M,'OutputType','llr','UnitAveragePower',true,'NoiseVariance',noise_var);
+    rec_bitsUncoded = qamdemod(rxSigUncoded,M,'OutputType','bit','UnitAveragePower',true,'NoiseVariance',noise_var);
+%scatterplot(rec_bits)
+%scatterplot(rec_bitsUncoded)
 
-        x_quant = (convertReal + 1i*convertImag);
-%scatterplot(x_quant)         
-    error_symbols = mean(abs(x_quant - modSig).^2);
+    %qamdemod demodulate (soft decision)
+    rec_bitsSoft = qamdemod(rxSig,M,'OutputType','approxllr','UnitAveragePower',true,'NoiseVariance',noise_var);
     
-    % Demodulate
-    x_quant_uunormalized = x_quant/L;
-%scatterplot(x_quant_uunormalized)
-%     txbits_I = 1/2*(real(x_quant_uunormalized)+1);
-%     txbits_Q = 1/2*(imag(x_quant_uunormalized)+1);
-    txbits_I = de2bi(int8((real(x_quant_uunormalized)+7)/2) ,3);
-    txbits_Q = de2bi(int8((imag(x_quant_uunormalized)+7)/2) ,3);
-    
-    hold = [txbits_I txbits_Q];
-    transHold = double(hold');
-    rec_bits = transHold(:);
     
     % Decode LDPC
-    demodSig = rec_bits;
-error_demod = mean(abs(demodSig' - dataConvert).^2);
-
-    %!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    rxData = ldpcDec(demodSig);  %something goes wrong here, error check (without noise) is ok up until this is executed
-
+    rxData = step(ldpcDec, rec_bits);%ldpcDec(rec_bits); 
+    rxDataSoft = step(ldpcDec, rec_bitsSoft);%ldpcDec(rec_bitsSoft); 
+    
     % Compute Error
-    rxDataHold = rxData.';
-    rxData = xor(rxDataHold,1); %for some reason a majority of bits are flipped???
-error_symbols = mean(abs(rxData - txbits).^2);
-
+    rxDataUncoded = rec_bitsUncoded;
+    %rxData = xor(rxDataHold,1); %for some reason a majority of bits are flipped???
+    error_final = mean(abs(rxData - txbits).^2);
+    error_finalUncoded = mean(abs(rxDataUncoded - txbits).^2);
+    
     [err,ber] = biterr(rxData,txbits);
+    [errSoft,berSoft] = biterr(rxDataSoft,txbits);
+    [errUncoded,berUncoded]= biterr(rxDataUncoded,txbits);
+    
     BER(k) = ber;
+    BERSoft(k) = berSoft;;
+    BERUncoded(k) = berUncoded;
+    
 end
 
-figure, semilogy(snrVec.',BER)
-xlabel('SNR (dB)')
+figure, semilogy(EbNo,BER)
+hold on
+semilogy(EbNo,BERSoft)
+hold on
+semilogy(EbNo,BERUncoded)
+legend('Coded BER', 'Coded BER Soft', 'Uncoded BER')
+xlabel('Eb/No (dB)')
 ylabel('BER')
+title('LDPC 64 QAM')
 
