8f6f045353f83df12cfa4d3ac9f51ebc4929fc56
Joseph Dieciedue <jdieciedue@utexas.edu>
2020-04-10 11:28:47 -0500

    Updated gradient descent function to output the gain estimation as well.

--- AGCExperiment/functions/AGCgrad.m
+++ AGCExperiment/functions/AGCgrad.m
@@ -1,15 +1,16 @@
-function output = AGCgrad(r)
-    %AGCGRAD Summary of this function goes here
+function [signal, estimation] = AGCgrad(r)
+    %AGCGRAD : 
     %   Detailed explanation goes here
     n=length(r);                           % number of steps in simulation
-    ds=1;                           % desired power of output
-    mu=0.001;                          % algorithm stepsize
-    a=zeros(n,1); a(1)=1;              % initialize AGC parameter
-    s=zeros(n,1);                      % initialize outputs             
+    ds=1;                                   % desired power of output
+    mu=0.01;                                % algorithm stepsize
+    a=zeros(n,1); a(1)=1;                   % initialize AGC parameter
+    s=zeros(n,1);                           % estimation of original             
     for k=1:n-1
         s(k)=a(k)*r(k);                       % normalize by a to get s
         a(k+1)=a(k)-mu*sign((s(k)^2)-(ds^2));       % average adaptive update of a(k)
     end
-    output = o;
+    signal = s;
+    estimation = a;
 end
 
