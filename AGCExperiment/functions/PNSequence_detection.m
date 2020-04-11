68ba33af094dd06b82e0b506280a3ee705de1bcf
AustinDuong <Duong_Austin@utexas.edu>
2020-04-10 22:10:47 -0500

    Updated PN training sequence parts

--- AGCExperiment/functions/PNSequence_detection.m
+++ AGCExperiment/functions/PNSequence_detection.m
@@ -1,16 +1,6 @@
-function PNSequence_detection(data, training_sequence)
+function [training_sequence, data] = PNSequence_detection(receivedSignal)
+% Separate the receivedSignal by separating the training sequence from the
+% rest of the data
 
-% Create Pseudonumber sequence object with certain properties
-%pnSequence = comm.PNSequence('Polynomial',[3 2 0],'SamplesPerFrame',7,'InitialConditions',[0 0 1]);
-
-% Generate the PN training sequence
-%training_sequence = pnSequence();
-%training_sequence = training_sequence';
-
-% Generate random received signal with embedded sequence
-% loc=30; r=25;
-% data=[sign(randn(1,loc-1)) training_sequence sign(randn(1,r))];  % generate random signal
-% sd=.25; data=data+sd*randn(size(data));              % add noise
-
-% Detect sequence and plot
-plot(abs(xcorr(data,training_sequence)).^2);
+training_sequence = receivedSignal(1,32);
+data = receivedSignal(33,end);
