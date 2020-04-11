c86d41ec0ac026bf4e06ee241efe2d06221683dd
carosheehy <carolynsheehy97@utexas.edu>
2020-04-11 11:25:54 -0500

    changed conversion to qs and 1s

--- AGCExperiment/functions/golay_sequence_detection.m
+++ AGCExperiment/functions/golay_sequence_detection.m
@@ -1,8 +1,16 @@
 % AGC_golay_sequence_detection
 % Detect Ga sequence within reveived signal
 
-function[retrieved_sequence, retrieved_data]= golay_sequence_detection(data)
+function[retrieved_sequence, retrieved_data]= golay_sequence_detection(data, training_sequence)
 
     retrieved_sequence = data(1:32);
     retrieved_data = data(33:end);
  
+    y=xcorr(training_sequence, data);      % do cross correlation
+    [m,ind]=max(y);                        % location of largest correlation
+    headstart=length(data)-ind+1;          % place where training sequence starts
+    
+   % retrieved_sequence = data(headstart : headstart+32);
+    %retrieved_data = data(headstart+33 : end);
+    
+        
--- AGCExperiment/functions/golay_sequence_generation.m
+++ AGCExperiment/functions/golay_sequence_generation.m
@@ -4,9 +4,9 @@
 function[data_in, training_sequence]= golay_sequence_generation(modulatedSignal)
     [Ga,Gb] = wlanGolaySequence(32);
     training_sequence = reshape(Ga, [1,32]);
-    for bit = 1: length(training_sequence)
-        if training_sequence(bit)== -1
-            training_sequence(bit) = 0;
-        end
-    end
+    %for bit = 1: length(training_sequence)
+       % if training_sequence(bit)== -1
+      %      training_sequence(bit) = 0;
+     %   end
+    %end
     data_in = [training_sequence modulatedSignal];
