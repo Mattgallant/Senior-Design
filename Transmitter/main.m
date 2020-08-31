b5e4dd982abb18ad609eede553a33d44f75a27c8
Matthew Gallant <31527991+Mattgallant@users.noreply.github.com>
2020-08-31 16:49:23 -0500

    Organized Transmitter/main.com

--- Transmitter/main.m
+++ Transmitter/main.m
@@ -3,3 +3,34 @@
 % Subteam: Matt, Jaino & Neel
 
 
+%% Input Data (Text File, String)
+
+
+%% Bitstream Conversion (Jaino)
+% text_to_bitstream
+
+
+%% Channel Encoding (Joseph) 
+% turbo_encoding
+
+
+%% Constellation Mapping (Jaino)
+% BPSK_mapping
+
+
+%% Training Sequence Injection (Carolyn)
+% golay_injection
+
+
+%% Pulse Shaping & Upsampling(Neel)
+% upsample_and_filter, srrc_filter
+
+
+%% Upconversion (Matt)
+% upconvert
+    wave = [1, 2, 3, 4];
+    upconverted_wave = upconvert(wave);
+
+%% Output to speaker (Matt)
+% transmitter_to_speaker
+    transmitter_to_speaker(upconverted_wave);
