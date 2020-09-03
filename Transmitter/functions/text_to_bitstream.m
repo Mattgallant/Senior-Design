61d888d528a8d6905cb477316b84d0d713e9ed1e
Jaino Vennatt <jainovennatt@Jainos-MacBook-Pro.local>
2020-09-02 20:35:06 -0500

    Added the input function to the transmitter functions

--- Transmitter/functions/text_to_bitstream.m
+++ Transmitter/functions/text_to_bitstream.m
@@ -1,7 +1,35 @@
-function [outputArg1,outputArg2] = text_to_bitstream(inputArg1,inputArg2)
-%TEXT_TO_BITSTREAM Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
 
+%TEXT_TO_BITSTREAM Function that converts a text document to a bitstream
+%   Function takes in a value that represents the number of bits to be sent
+%   and then opens up the alice in wonderland text file and converts it to
+%   bits until the specified amount is reached
+
+function [sourceCharacters,sendableBits] = text_to_bitstream(numberOfBits)
+    %Open source file
+    filePointer= fopen('alice_in_wonderland.txt');
+    %Read in characters of book
+    fileValues= fscanf(filePointer,'%c');
+    %Convert character values to binary
+    binaryValues = dec2bin(fileValues);
+    %CharactersBeingTested divided by 8 because of size of character is 8bit
+    sourceCharacters= fileValues(1:fix(numberOfBits/8)); %in ascii
+    %disp(sourceCharacters);
+    %Get the number of Rows and Columns of Binary Data
+    [rows,columns] = size(binaryValues);
+    %Reshapes to one row, and necessary number of Columns
+    reshapedBinaryValuesArray= reshape(binaryValues', 1, rows*columns);
+    %Creates the array for the number of bits to send 
+    characterConversion= reshapedBinaryValuesArray(1:numberOfBits);
+    sendableBits= zeros(1,numberOfBits);    
+    zeroValue='0';
+    oneValue='1';
+    %Converts from character binary values to actual double binary values
+    %Enables better use of modulation and other number based libraries
+    for i=1:1:numberOfBits
+        if(strcmp(characterConversion(i),oneValue))
+            sendableBits(i)=1;
+        elseif(strcmp(characterConversion(i),zeroValue))
+            sendableBits(i)=0;
+        end        
+    end
+end
