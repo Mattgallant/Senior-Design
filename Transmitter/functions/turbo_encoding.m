c44dbd1dedc3c8e369ae5f5a0f62cae4804bc33d
Matthew Gallant <31527991+Mattgallant@users.noreply.github.com>
2020-09-14 16:05:04 -0500

    Fixed problem with Turbo Encoding only accepting 6144 bits, used different library

--- Transmitter/functions/turbo_encoding.m
+++ Transmitter/functions/turbo_encoding.m
@@ -1,8 +1,10 @@
 function [output_bits] = turbo_encoding(input_bits)
 %TURBO_ENCODING Turbo encodes input data and returns the coded data
-%   Uses the LTE library and takes INPUT_BITS, turbo encodes it and returns
-%   the encoded bitstream. As of now, turbo encode seems to produce ~ 2
-%   bits for every 1 bit of input. INPUT_BITS must be a multiple of 8!
-    output_bits = lteTurboEncode(input_bits);
+%   Uses the LTE Comm library and takes INPUT_BITS, turbo encodes it and returns
+%   the encoded bitstream. Coding rate is ~ 1/3. INPUT_BITS must be a column vector!
+    rng default
+    turboEncoder = comm.TurboEncoder('InterleaverIndicesSource','Input port');
+    output_bits = step(turboEncoder, input_bits, randperm(length(input_bits)));
+    %disp("Coding rate is approximately: " + rat(length(input_bits)/length(output_bits), .001));
 end
 
--- Transmitter/main.m
+++ Transmitter/main.m
@@ -4,43 +4,47 @@
 
 %% Input Data (Text File, String)
     file_pointer= fopen("lorem.txt"); %Open file to read from
-    read_length = 6144;
+    read_length = 10000;
 
 %% Bitstream Conversion (Jaino)
 % text_to_bitstream
     [source_characters, sendable_bits] = text_to_bitstream(file_pointer, read_length);
-    disp(sendable_bits) %Currently a row vector
+    %disp(sendable_bits) %Currently a row vector
+    %disp(source_characters)
 
 %% Channel Encoding (Joseph) 
 % turbo_encoding
-   encoded_bits = turbo_encoding(sendable_bits);
-   disp(encoded_bits); %Currently a col vector
+   encoded_bits = turbo_encoding(sendable_bits.');
+   %disp(encoded_bits); %Currently a col vector
+
+   
 
 %% Constellation Mapping (Jaino)
 % BPSK_mapping
-    modulated_bits = BPSK_mapping(encoded_bits);
-    disp(modulated_bits); %Currently a row vector
-    %modulated_bit_length = length(modulated_bits);
+%     modulated_bits = BPSK_mapping(encoded_bits);
+%     disp(modulated_bits); %Currently a row vector
+%     modulated_bit_length = length(modulated_bits);
+%     disp(modulated_bit_length)
 
 %% Training Sequence Injection (Carolyn)
 % golay_injection
-    [bitstream_with_injection, training_sequence] =  golay_injection(modulated_bits, 128);
+%     [bitstream_with_injection, training_sequence] =  golay_injection(modulated_bits, 128);
 
 %% Pulse Shaping & Upsampling(Neel)
 % upsample_and_filter, srrc_filter
 %Filter properties
-    oversampling_factor = 4; % Number of samples per symbol (oversampling factor)
-    span = 10; % Filter length in symbols
-    rolloff = .1; % Filter rolloff factor
-    dataRate = 500; %Data Rate in symbols/sec
-    [pulse_shaped_signal] = srrc_filter(bitstream_with_injection,span,rolloff,oversampling_factor,dataRate);
+%     oversampling_factor = 4; % Number of samples per symbol (oversampling factor)
+%     span = 10; % Filter length in symbols
+%     rolloff = .1; % Filter rolloff factor
+%     dataRate = 500; %Data Rate in symbols/sec
+%     [pulse_shaped_signal] = srrc_filter(bitstream_with_injection,span,rolloff,oversampling_factor,dataRate);
 
 %% Upconversion (Matt)
 % upconvert
     %wave = randi(10, 1, 5*44100);       % For testing purposes
-    upconverted_wave = upconvert(real(pulse_shaped_signal));
+    %upconverted_wave = upconvert(real(pulse_shaped_signal));
 
 %% Output to speaker (Matt)
 % transmitter_to_speaker
-    transmitter_to_speaker(upconverted_wave);
+    %transmitter_to_speaker(upconverted_wave);
    
