2e4721210aca90642e501955a88f02bf4b78ba62
Phat Le <phatle10@gmail.com>
2020-09-30 16:05:18 -0500

    updated DFE test script with additional plots

--- Random Scripts/DecisionFeedbackEqualizerTest.m
+++ Random Scripts/DecisionFeedbackEqualizerTest.m
@@ -21,17 +21,23 @@ dfeq = comm.DecisionFeedbackEqualizer('Algorithm','LMS', ...
 release(dfeq)
 jj = 1;
 figure
+chCo=[0.5 1 -0.6];              % define channel
 for ii = 1:numPkts
     b = randi([0 M-1],numDataSymbols,1);
     dataSym = bpsk(b);                          %changed from qpsk to bpsk
     packet = [trainingSymbols;dataSym];
     channel = 1;
-    rx = awgn(packet*channel,SNR);
-    [~,err] = dfeq(rx,trainingSymbols);
+    rx = filter(chCo,1,packet);             %awgn(packet*channel,SNR); %channel simulation line
+    [y,err] = dfeq(rx,trainingSymbols);
     if (ii ==1 || ii == 2 ||ii == numPkts)      %BER worse than QPSK, acts like being reset every packet
-        subplot(3,1,jj)
-        plot(abs(err))
-        ylim([0 1])
+        %subplot(3,1,jj)
+        %plot(abs(err))
+        
+        scatter(complex([1:length(y)])', y)
+        %scatter(complex([1:length(dataSym)])', dataSym)
+        %scatter(complex([1:length(rx)])', rx)
+        %ylim([0 1])
+        ylim([-1 1])
         title(['Packet # ',num2str(ii)])
         xlabel('Symbols');
         ylabel('Error Magnitude');
