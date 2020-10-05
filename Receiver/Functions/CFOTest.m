84b358ad631bd8149525fcd78e79b7c2dade0e08
Matthew Gallant <31527991+Mattgallant@users.noreply.github.com>
2020-10-05 11:06:18 -0500

    Created simulation folder and setup a simulation channel

--- Receiver/Functions/CFOTest.m
+++ Receiver/Functions/CFOTest.m
@@ -6,17 +6,21 @@ clear
 n = 1000; % Length of message
 msg = randi([0,1],n,1); % Random message of bits
 bpskMod = comm.BPSKModulator;   % BPSK mod object
-bpskMod.PhaseOffset = pi/4;    % Set phase offset
-modData = bpskMod(msg);     % Modulate message w/ offset
+% bpskMod.PhaseOffset = pi/4;     % Set phase offset
 
-snr = 5;    % Signal-to-Noise Ratio
-rxSig = awgn(modData,snr);   % Add white noise
+pfo = comm.PhaseFrequencyOffset('PhaseOffset',45, ...
+    'FrequencyOffset',1e6);
+modData = bpskMod(msg);         % Modulate message w/ offset
+
+modSigOffset = pfo(modData);
+snr = 3;    % Signal-to-Noise Ratio
+rxSig = awgn(modSigOffset,snr);   % Add white noise
 scatterplot(rxSig(1:end));
-title('Constellation with CFO of \pi/4')
+title('Constellation with CFO')
 
-[outSig, phaseErr] = CarrierFrequencyOffset(rxSig);
-disp(phaseErr)
-scatterplot(outSig(1:end));
+outSig = CarrierFrequencyOffset(rxSig');
+disp(outSig)
+scatterplot(outSig);
 title('Constellation after Carrier Recovery')
 
 
