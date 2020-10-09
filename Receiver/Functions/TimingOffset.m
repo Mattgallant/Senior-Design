b0204f23cc81e9ede4f14c7ef3fcadd4e7609649
Phat Le <phatle10@gmail.com>
2020-10-09 14:17:11 -0500

    adaptive TO just incase, not tested

--- Receiver/Functions/TimingOffset.m
+++ Receiver/Functions/TimingOffset.m
@@ -1,9 +1,17 @@
 %%%%%%%%%%%%%%%TODO%%%%%%%%%%%%%%%
 
-function [outputArg1,outputArg2] = TimingOffset(inputArg1,inputArg2)
-%TIMINGOFFSET Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function [correctedSignal] = TimingOffset(signal, sps)
+%TIMINGOFFSET - corrects the symbol timing of a signal without being aided
+%by known data
+%   input: signal - signal to be corrected for symbol timing
+%          sps - samples per symbol (oversampling factor)
+%   output: correctedSignal - the signal with corrected timing offset
+symbolSync = comm.SymbolSynchronizer(...
+    'SamplesPerSymbol',sps, ...
+    'NormalizedLoopBandwidth',0.01, ...
+    'DampingFactor',1.0, ...                %these 2 values can be tuned to improve offset
+    'TimingErrorDetector','Early-Late (non-data-aided)');   %early-late vs gardner 
+
+[correctedSignal,~] = symbolSync(signal); %Note: ~ is err
 end
 
