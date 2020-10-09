e5add4e8e7be944edbf234306866a95e676e81af
Phat Le <phatle10@gmail.com>
2020-10-09 15:25:46 -0500

    lvl3 integrates upconversion and downconversion

--- Receiver/Functions/downconvert.m
+++ Receiver/Functions/downconvert.m
@@ -1,8 +1,8 @@
-function [upconverted_wave] = upconvert(wave)
-%UPCONVERT Takes WAVE and upconverts it to desired carrier frequency
+function [downconverted_wave] = downconvert(wave)
+%DOWNCONVERT Takes WAVE and downconverts it to desired carrier frequency
 %   Takes SRRC filtered digital signal (WAVE) and multiplies it by an
-%   appropriate carrier wave. The upconverted wave should be a sine wave
-%   and have center frequency of 9kHz. Returns the UPCONVERTED_WAVE.
+%   appropriate carrier wave. The downconverted wave should be a sine wave
+%   and have center frequency of 9kHz. Returns the DOWNCONVERTED_WAVE.
     center_frequency = 9000;    % As per project definition, center freq 9kHz
     Fs = 44100;                 % Sampling Frequency of 44.1kHz (Samples per Second)
     dt = 1/Fs;                  % Seconds per sample
@@ -21,8 +21,14 @@ function [upconverted_wave] = upconvert(wave)
     figure(2);
     plot(t,wave.*carrier);
     xlabel('time (in seconds)');
-    title('Carrier Wave Modulated w/ Data');
+    title('Downconverted Wave w/ Data');
     zoom xon;
-    upconverted_wave = wave.* carrier;
+    %downconverted = 2 .*( wave.* carrier);   %*2 for scaling
+    downconverted_wave = 2 .*( wave.* carrier);
+    
+    
+    %LPF ---!!!!!!!!!!!!!!!! currently causing bit error ???
+    x = load('please.mat')      %done with filter design/fdatool
+    %downconverted_wave = filter(x.FilterTest, 1, downconverted);                % calculation of LPF impulse response
 end
 
---
+++ Simulation/lvl3.m
@@ -0,0 +1,80 @@
+%% LVL 3
+%  Implemented - Input Data, TXT-To-Bitstream, Mapping, Channel Encoding, SRRC, Upconversion, 
+%  Downconversion, Match Filtering, Decoding, Demapping, Bitstream-To-TXT
+
+%  Not yet implemented - Timing Offset, Carrier Offset, Training detection/injection,
+%  AGC, Channel estimation/equalization
+%% Transmitter
+%  Input Data
+
+file_pointer= fopen("lorem.txt"); 
+read_length_characters = 2001; 
+
+%  TXT-To-Bitstream
+
+[source_characters, sendable_bits] = text_to_bitstream(file_pointer, read_length_characters);
+
+length(sendable_bits)
+
+%  Channel Encoding
+
+encoded_bits = turbo_encoding(sendable_bits.');
+
+length(encoded_bits);
+%  Constellation Mapping
+
+modulated_bits = BPSK_mapping(encoded_bits);
+
+%  SRRC Filtering
+
+rolloff = 0.25;
+span = 10;
+sps = 6;
+M = 2;
+k = log2(M);
+
+rrcFilter = rcosdesign(rolloff, span, sps,'sqrt');
+
+pulseShaped = upfirdn(modulated_bits, rrcFilter, sps);
+
+%Upconversion
+txSig = upconvert(pulseShaped);
+
+%% Channel
+
+EbNo = 7;
+snr = EbNo + 10*log10(k) - 10*log10(sps);
+rxSig = awgn(txSig, snr, 'measured');
+
+
+%% Reciever
+
+%Downconversion
+downconverted = downconvert(rxSig);
+
+%  Match (SRRC) Filtering (NOT WORKING)
+
+rxFilt = upfirdn(downconverted, rrcFilter, 1, sps);
+match_filtered_signal = rxFilt(span+1:end-span);
+
+length(match_filtered_signal)
+%  Constellation DeMapping
+
+demodulated_bits =  Demodulation(match_filtered_signal);
+demodulated_bits = demodulated_bits(:);
+
+%  Channel Decoding
+
+decoded_bits = TurboDecoding(demodulated_bits);
+
+length(decoded_bits)
+%  Bitstream-To-TXT
+
+%text = Bitstream_to_Text(decoded_bits);
+
+%% Analysis
+
+%  BER
+[number, ratio] = biterr(sendable_bits(:), decoded_bits);
+disp(number);
+
