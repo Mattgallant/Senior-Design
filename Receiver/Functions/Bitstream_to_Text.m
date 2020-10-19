c2e11c312d5dbb0b1ff80c50af3ff4ba949c725a
Matthew Gallant <31527991+Mattgallant@users.noreply.github.com>
2020-10-19 15:28:34 -0500

    Updated Transmitter and Receiver main scripts

--- Receiver/Functions/Bitstream_to_Text.m
+++ Receiver/Functions/Bitstream_to_Text.m
@@ -4,11 +4,11 @@
 %   and converts it to a text file
 
 function [textValues] = Bitstream_to_Text(bitstream)
-    disp(size(bitstream))
+%     disp(size(bitstream))
     reshapedArray = reshape(bitstream(:), 7, []).';  
-    disp(size(reshapedArray))
+%     disp(size(reshapedArray))
     binaryCharArray= num2str(reshapedArray);
-    disp(size(binaryCharArray))
+%     disp(size(binaryCharArray))
     decimalAsciiValues = bin2dec(binaryCharArray);
     textValues= char(decimalAsciiValues).';
 end
